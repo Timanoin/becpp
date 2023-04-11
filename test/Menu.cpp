3f4d1065eb99d12026ee3cf7485d33a5d74092f7
Arthur Gautheron <gauthero@etud.insa-toulouse.fr>
2023-04-11 09:48:59 +0200

    push
    Co-authored-by: Olivier Lagrost <Lagrost@users.noreply.github.com>

---
+++ test/Menu.cpp
@@ -0,0 +1,37 @@
+#include "Menu.h"
+
+Menu::Menu() : listOptions(), cursor(listOptions.begin()) {}
+
+void Menu::addOption(MenuOption* option)
+{
+  listOptions.push_back(option);
+}
+
+void Menu::addOption(MenuOption* option, int pos)
+{
+  menuIt it;
+  for (int i = 0; i < pos; i++)
+  {
+    it++;
+  }
+  listOptions.insert(it, option);
+}
+
+void Menu::next(){
+  if (++cursor == listOptions.end()){
+    cursor = listOptions.begin();
+  }    
+};
+
+void Menu::prev(){
+  if (--cursor == listOptions.begin()){
+    cursor = listOptions.end();
+  }
+};
+
+// TODO:
+void Menu::select()
+{
+  
+}
+void Menu::back();
---
+++ test/MenuOption.cpp
@@ -0,0 +1,10 @@
+#include "MenuOption.h"
+#include "images/beta.xbm"
+#include "images/omega.xbm"
+#include "images/phi.xbm"
+#include "images/psi.xbm"
+
+void MenuOption::display()
+{
+  
+}
